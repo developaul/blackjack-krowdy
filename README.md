178dd523de00fc647be85a81d9db9de95e56b60b
Paul Chávez <paulchavezromero20@gmail.com>
2023-02-25 18:18:59 -0500

    Funcionalidad de la app agregada

--- README.md
+++ README.md
@@ -8,9 +8,13 @@
     - Generar puntos de jugadores -> Completado
   3.2 Funcionalidad "Pedir Carta"
     - Obtener siguiente carta -> Completado
-    - Acumular los puntos -> 
-    - Crear carta 
-    - Deshabilitar botones
-    - Pasar turno a computadora
-  3.3 FUncionalidad "Detener"
+    - Acumular los puntos -> Completado
+    - Crear carta -> Completado
+    - Deshabilitar botones -> Completado
+    - Pasar turno a computadora -> Completado
+      - Crear funcion "turnoComputadora" -> Completado
+      - Crear function "determinarGanador" -> Completado 
+  3.3 Funcionalidad "Detener"
+    - Deshabilitar botones -> Completado
+    - Ejecutar funcion turnoComputadora -> Completado
 4. Desplegarlo en github pages
--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 const btnNuevoJuego = document.querySelector('#btnNuevoJuego'),
   btnPedirCarta = document.querySelector('#btnPedirCarta'),
-  btnDetener = document.querySelector('#btnDetener')
-
+  btnDetener = document.querySelector('#btnDetener'),
+  puntosHTML = document.querySelectorAll('small'),
+  cartasDeJugadores = document.querySelectorAll('.cards')
 
 let baraja = []
 let puntosDeJugadores = []
@@ -30,19 +31,30 @@ const crearBaraja = () => {
 
 
 const init = (cantidadJugadores = 2) => {
-  
   baraja = crearBaraja()
+  puntosDeJugadores = []
 
   for(let i = 0; i < cantidadJugadores; i++) {
     puntosDeJugadores.push(0)
   }
   
+  for(let puntosDeJugador in puntosDeJugadores) {
+    puntosHTML[puntosDeJugador].textContent = 0;
+    cartasDeJugadores[puntosDeJugador].textContent = '';
+  }
+
   habilitarBotones()
 }
 
 const habilitarBotones = () => {
+  console.log('se habilitaron botones')
   btnPedirCarta.disabled = false
   btnDetener.disabled = false
+}
+
+const deshabilitarBotones = () => {
+  btnPedirCarta.disabled = true
+  btnDetener.disabled = true
 }
 
 const obtenerCarta = () => { 
@@ -52,20 +64,64 @@ const obtenerCarta = () => {
 }
 
 const obtenerValorDeCarta = (carta) => {
-  console.log("🚀 ~ file: scripts.js:55 ~ obtenerValorDeCarta ~ carta:", carta)
+  const valor = carta.substring(0, carta.length - 1)
 
-
-  // const valor = 
-
-
+  return !isNaN(valor) ? Number(valor) : 
+    valor === 'A' ? 11 : 10
 }
 
 const acumularPuntos = ({carta, turno}) => {
-  const valorDeCarta = obtenerValorDeCarta(carta)
+  puntosDeJugadores[turno] += obtenerValorDeCarta(carta)
+  puntosHTML[turno].textContent = puntosDeJugadores[turno]
 
+  return puntosDeJugadores[turno]
+}
 
-  // puntosDeJugadores[turno] = puntosDeJugadores[turno] + 
+const crearCarta = ({carta, turno}) => {
+  const imagen = document.createElement('img');
+  imagen.src = `assets/${carta}.png`
+  imagen.classList.add('carta')
+  cartasDeJugadores[turno].append(imagen)
+}
 
+const determinarGanador = ([ puntosDeJugador, puntosDeComputadora ]) => {
+  setTimeout(() => {
+    if(puntosDeJugador > 21) {
+      alert('Computadora gana!')
+      return
+    }
+
+    if(puntosDeComputadora > 21) {
+      alert('Jugador Gana!')
+      return
+    }
+
+    if(puntosDeJugador === puntosDeComputadora) {
+      alert('Nadie gana :(')
+      return
+    }
+
+    if(puntosDeJugador > puntosDeComputadora) {
+      alert('Jugador gana!')
+      return
+    }
+
+    alert('Computadora gana!')
+  }, 400)
+}
+
+const turnoComputadora = (puntosMinimos) => {
+  let puntosDeComputadora = 0
+
+  const turno = puntosDeJugadores.length - 1
+
+  do {
+    const carta = obtenerCarta()
+    puntosDeComputadora = acumularPuntos({ carta, turno })
+    crearCarta({ carta, turno })
+  } while(puntosDeComputadora < puntosMinimos && puntosMinimos <= 21)
+
+  determinarGanador(puntosDeJugadores)
 }
 
 btnNuevoJuego.addEventListener('click', () => {
@@ -74,7 +130,16 @@ btnNuevoJuego.addEventListener('click', () => {
 
 btnPedirCarta.addEventListener('click', () => {
   const carta = obtenerCarta()
+  const puntosDeJugador = acumularPuntos({ carta, turno: 0 })
+  crearCarta({ carta, turno: 0 })
 
-  acumularPuntos({ carta, turno: 0 })
+  if(puntosDeJugador < 21) return
 
+  deshabilitarBotones()
+  turnoComputadora(puntosDeJugador)
+})
+
+btnDetener.addEventListener('click', () => {
+  deshabilitarBotones()
+  turnoComputadora(puntosDeJugadores[0])
 })
